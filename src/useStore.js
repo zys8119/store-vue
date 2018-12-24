e26c4ed1768e7895109b0f83414eeb6decc26a8c
zys8119 <770959294@qq.com>
2018-12-24 11:39:54 +0800

    v1.2.5 删除工具集成vuex

--- src/useStore.js
+++ src/useStore.js
@@ -2,10 +2,6 @@ import { mapActions, mapGetters } from 'vuex'
 import utils from './utils/utils'
 const plugin = {
     install (Vue) {
-        try {
-            utils.action = mapActions(['action']).action
-            utils.airforce = mapGetters(['airforce']).airforce
-        }catch (e){}
         Vue.prototype.$utils = utils;
         Vue.mixin({
             methods:mapActions(['action']),
